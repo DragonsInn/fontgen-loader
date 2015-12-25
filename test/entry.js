f58ff3430d29d20365e16f2656385d7b7e0af398
Peter K <petrk@wix.com>
2015-12-25 15:35:20 +0200

    Add support for glob in files array, with according directory (context) watch

--- test/entry.js
+++ test/entry.js
@@ -1,3 +1,13 @@
-var font = require("./octicons.font");
+require("./octicons.font");
+require("./octicon2/octicon2.font");
+require("./octicons-json.font");
 
-console.log(font);
+document.write('<span class="octicons octicons-alert"></span>  ');
+document.write('<span class="octicon2 octicon2-alignment-align"></span>  ');
+document.write('<span class="octicon2 octicon2-arrow-right"></span>  ');
+document.write('<span class="octicon2 octicon2-arrow-right2"></span>  ');
+document.write('<span class="octicon2 octicon2-arrow-left"></span>  ');
+document.write('<span class="octicon2 octicon2-alignment-aligned-to"></span>  ');
+document.write('<span class="octicon octicon-alert"></span>  ');
+document.write('<span class="octicon-json octicon-json-alert"></span>  ');
+
--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,18 +1,20 @@
+var path = require('path');
 module.exports = {
+    context: path.resolve(__dirname),
     entry: "./entry.js",
     output: {
         filename: "bundle.js",
-        path: "./build"
+        path: path.join(__dirname, 'build')
     },
     module: {
         loaders: [
             {
-                test: /\.font\.js$/,
-                loader: "style!css!"+require.resolve("../")
-            },{
+                test: /\.font\.(js|json)$/,
+                loader: "style!css!" + require.resolve("../")
+            }, {
                 test: /\.(woff|eot|ttf|svg)$/,
                 loader: "url"
             }
         ]
     }
-}
+};
